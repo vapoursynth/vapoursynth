c85edcb8f6df67f16cc7fedfd5a34fc8a204339c
Fredrik Mellbin <fredrik.mellbin@gmail.com>
2012-09-16 19:59:39 +0000

    More useful files
    git-svn-id: http://vapoursynth.googlecode.com/svn/trunk@3 f9120d27-2007-6f97-8312-0f4ebfa7710f

---
+++ setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup
+from distutils.extension import Extension
+from Cython.Distutils import build_ext
+
+setup(
+    cmdclass = {'build_ext': build_ext},
+    ext_modules = [Extension("vapoursynth", ["src/cython/vapoursynth.pyx"])]
+)
