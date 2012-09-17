2d46b9bb31f42e41382a56b2b8b88aea2935c8e1
Fredrik Mellbin <fredrik.mellbin@gmail.com>
2012-09-17 16:55:39 +0000

    Add missing license headers
    git-svn-id: http://vapoursynth.googlecode.com/svn/trunk@18 f9120d27-2007-6f97-8312-0f4ebfa7710f

--- src/cython/libinclude.h
+++ src/cython/libinclude.h
@@ -1,3 +1,20 @@
+//  Copyright (c) 2012 Fredrik Mellbin
+//
+//  This file is part of VapourSynth.
+//
+//  VapourSynth is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU Lesser General Public License as
+//  published by the Free Software Foundation, either version 3 of the
+//  License, or (at your option) any later version.
+//
+//  VapourSynth is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with VapourSynth.  If not, see <http://www.gnu.org/licenses/>.
+
 #ifndef LIBINCLUDE_H
 #define LIBINCLUDE_H
 #pragma comment(lib,"vscore.lib")
