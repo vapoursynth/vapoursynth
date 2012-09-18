1fc8471a677693570a33951373050238aa921ee4
Fredrik Mellbin <fredrik.mellbin@gmail.com>
2012-09-18 14:24:01 +0000

    Redo all license headers since the official FSF suggestion wants to make LGPL sound like GPL. The text is now copied from libav.
    git-svn-id: http://vapoursynth.googlecode.com/svn/trunk@24 f9120d27-2007-6f97-8312-0f4ebfa7710f

--- src/core/cachefilter.h
+++ src/core/cachefilter.h
@@ -1,19 +1,22 @@
-//  Copyright (c) 2012 Fredrik Mellbin
-//
-//  This file is part of VapourSynth.
-//
-//  VapourSynth is free software: you can redistribute it and/or modify
-//  it under the terms of the GNU Lesser General Public License as
-//  published by the Free Software Foundation, either version 3 of the
-//  License, or (at your option) any later version.
-//
-//  VapourSynth is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
-//
-//  You should have received a copy of the GNU General Public License
-//  along with VapourSynth.  If not, see <http://www.gnu.org/licenses/>.
+/*
+* Copyright (c) 2012 Fredrik Mellbin
+*
+* This file is part of VapourSynth.
+*
+* VapourSynth is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
+*
+* VapourSynth is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with Libav; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+*/
 
 #ifndef CACHEFILTER_H
 #define CACHEFILTER_H
