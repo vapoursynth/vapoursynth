668e04f8e1f3f8c156c7b1fd5b45ea46034e739f
Alex Rønne Petersen <alex@lycus.org>
2012-09-18 16:18:17 +0000

    Remove PREFIX definitions in source code.
    git-svn-id: http://vapoursynth.googlecode.com/svn/trunk@27 f9120d27-2007-6f97-8312-0f4ebfa7710f

--- src/core/vsstdlib_asm.asm
+++ src/core/vsstdlib_asm.asm
@@ -16,7 +16,6 @@
 ; License along with Libav; if not, write to the Free Software
 ; Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
 
-%define PREFIX
 %include "x86inc.asm"
 
 SECTION .text
