e3e861fbcfb5efe6d5a0fbc46880efbeaa9d89ce
Alex Rønne Petersen <alex@lycus.org>
2012-09-20 23:21:04 +0000

    Add include paths to setup.py.
    git-svn-id: http://vapoursynth.googlecode.com/svn/trunk@61 f9120d27-2007-6f97-8312-0f4ebfa7710f

--- setup.py
+++ setup.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
+from os import pardir
+from os.path import join
 from distutils.core import setup
 from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
 setup(
     cmdclass = {'build_ext': build_ext},
-    ext_modules = [Extension("vapoursynth", ["src/cython/vapoursynth.pyx"])]
+    ext_modules = [Extension('vapoursynth', ['src/cython/vapoursynth.pyx'],
+                             include_dirs = ['.', join(pardir, 'src', 'cython')])]
 )
