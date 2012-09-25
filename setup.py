7be9fc699e4888e36cb5e74dbdb253fa9e6e8415
Fredrik Mellbin <fredrik.mellbin@gmail.com>
2012-09-25 12:56:16 +0000

    Add python module information
    git-svn-id: http://vapoursynth.googlecode.com/svn/trunk@88 f9120d27-2007-6f97-8312-0f4ebfa7710f

--- setup.py
+++ setup.py
@@ -7,6 +7,16 @@ from distutils.extension import Extension
 from Cython.Distutils import build_ext
 
 setup(
+    name = "VapourSynth",
+    description = "A frameserver for the 21st century",
+    url = "http://www.vapoursynth.com/",
+    download_url = "http://code.google.com/p/vapoursynth/",
+    author = "Fredrik Mellbin",
+    author_email = "fredrik.mellbin@gmail.com",
+    license = "LGPL 2.1 or later",
+    version = "1.0.0",
+    long_description = "A portable replacement for Avisynth",
+    platforms = "All",
     cmdclass = {'build_ext': build_ext},
     ext_modules = [Extension('vapoursynth', ['src/cython/vapoursynthpp.pyx'],
                              libraries=["vapoursynth"],
