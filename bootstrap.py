7127a4e8b676a30af44f0c871ff6c91bed2645d1
Alex Rønne Petersen <alex@lycus.org>
2012-10-20 09:06:34 +0000

    Minor chmod fix in bootstrap.py.
    git-svn-id: http://vapoursynth.googlecode.com/svn/trunk@260 f9120d27-2007-6f97-8312-0f4ebfa7710f

--- bootstrap.py
+++ bootstrap.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python
 
-import os
-import stat
-import urllib2
+import os, stat, urllib2
 
 f = urllib2.urlopen('http://waf.googlecode.com/files/waf-1.7.5')
 
 with open('waf', 'wb') as waf:
     waf.write(f.read())
 
-os.chmod('waf', stat.S_IXUSR)
+os.chmod('waf', os.stat('waf').st_mode | stat.S_IXUSR)
