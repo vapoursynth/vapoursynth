8057c76a9396cc3bffbbb24a1f2071282a4e1514
Fredrik Mellbin <fredrik.mellbin@gmail.com>
2013-07-26 15:03:01 +0200

    Fix the license header mess in VSFS and don't mention avisynth linking

--- src/vsfs/vsfs.cpp
+++ src/vsfs/vsfs.cpp
@@ -2,6 +2,7 @@
 //
 // VapourSynth modifications Copyright 2012-2013 Fredrik Mellbin
 //
+// Original code from:
 // Avisynth v2.5.  Copyright 2008 Ben Rudiak-Gould et al.
 // http://www.avisynth.org
 
@@ -19,23 +20,6 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA, or visit
 // http://www.gnu.org/copyleft/gpl.html .
-//
-// Linking Avisynth statically or dynamically with other modules is making a
-// combined work based on Avisynth.  Thus, the terms and conditions of the GNU
-// General Public License cover the whole combination.
-//
-// As a special exception, the copyright holders of Avisynth give you
-// permission to link Avisynth with independent modules that communicate with
-// Avisynth solely through the interfaces defined in avisynth.h, regardless of the license
-// terms of these independent modules, and to copy and distribute the
-// resulting combined work under terms of your choice, provided that
-// every copy of the combined work is accompanied by a complete copy of
-// the source code of Avisynth (the version of Avisynth used to produce the
-// combined work), being distributed under the terms of the GNU General
-// Public License plus this exception.  An independent module is a module
-// which is not derived from or based on Avisynth, such as 3rd-party filters,
-// import and export plugins, or graphical user interfaces.
-
 
 #include "vsfsincludes.h"
 #include <fstream>
--- src/vsfs/vsfsavi2.cpp
+++ src/vsfs/vsfsavi2.cpp
@@ -1,11 +1,8 @@
 // vsfsavi2.cpp : VapourSynth Virtual File System
 //
-// VapourSynth modifications Copyright 2012 Fredrik Mellbin
-// This license header makes no sense. It was copied from
-// Avisynth and says that people who definitely never touched
-// the file claim copyright on it. Anyway, below is the complete
-// original notice, enjoy the GPL.
+// VapourSynth modifications Copyright 2012-2013 Fredrik Mellbin
 //
+// Original code from:
 // Avisynth v2.5.  Copyright 2008-2010 Ben Rudiak-Gould et al.
 // http://www.avisynth.org
 
@@ -23,24 +20,6 @@
 // along with this program; if not, write to the Free Software
 // Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA, or visit
 // http://www.gnu.org/copyleft/gpl.html .
-//
-// Linking Avisynth statically or dynamically with other modules is making a
-// combined work based on Avisynth.  Thus, the terms and conditions of the GNU
-// General Public License cover the whole combination.
-//
-// As a special exception, the copyright holders of Avisynth give you
-// permission to link Avisynth with independent modules that communicate with
-// Avisynth solely through the interfaces defined in avisynth.h, regardless of
-// the license terms of these independent modules, and to copy and distribute
-// the resulting combined work under terms of your choice, provided that
-// every copy of the combined work is accompanied by a complete copy of
-// the source code of Avisynth (the version of Avisynth used to produce the
-// combined work), being distributed under the terms of the GNU General
-// Public License plus this exception.  An independent module is a module
-// which is not derived from or based on Avisynth, such as 3rd-party filters,
-// import and export plugins, or graphical user interfaces.
-
-
 
 //----------------------------------------------------------------------------
 // Implementation Notes.
