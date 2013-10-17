56e570b5aa0e628a2449c71fbe6dee5f1c5ab52e
Fredrik Mellbin <fredrik.mellbin@gmail.com>
2013-10-17 21:24:03 +0200

    Remnove floating point use in cache filter

--- src/core/cachefilter.cpp
+++ src/core/cachefilter.cpp
@@ -34,10 +34,10 @@ VSCache::CacheAction VSCache::recommendSize() {
         return caNoChange; // not enough requests to know what to do so keep it this way
     }
 
-    if ((float)nearMiss / total > 0.2) { // growing the cache would be beneficial
+    if ((nearMiss*10) / total >= 2) { // growing the cache would be beneficial
         clearStats();
         return caGrow;
-    } else if ((float)farMiss / total > 0.9) { // probably a linear scan, no reason to waste space here
+    } else if ((farMiss*10) / total >= 9) { // probably a linear scan, no reason to waste space here
         clearStats();
         return caShrink;
     } else {
