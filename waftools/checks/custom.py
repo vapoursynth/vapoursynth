517cc83aff398b62073abbcd2d23bbaae556d3ae
Martin Herkt <lachs0r@srsfckn.biz>
2014-03-04 12:42:51 +0100

    waf: fix Python 3 check

--- waftools/checks/custom.py
+++ waftools/checks/custom.py
@@ -1,6 +1,6 @@
 from waftools.inflectors import DependencyInflector
 from waftools.checks.generic import *
-from waflib import Utils
+from waflib import Utils, Errors
 import os
 
 __all__ = ["check_python", "check_cpu_x86", "check_cpu_x86_64"]
@@ -9,14 +9,12 @@ def check_python(ctx, dependency_identifier):
     ctx.find_program(['python3', 'python'], var = 'PYTHON')
     ctx.load('python')
 
-    ctx.check_python_version()
-
-    ver = int(ctx.env.PYTHON_VERSION.split('.')[0])
-    if (ver == 3):
+    try:
+        ctx.check_python_version((3, 0, 0))
         ctx.check_python_headers()
         return True
-
-    return False
+    except Errors.WafError:
+        return False
 
 def check_cpu_x86(ctx, dependency_identifier):
     return ctx.env.DEST_CPU in ['x86', 'x86_64', 'x64', 'amd64', 'x86_amd64']
