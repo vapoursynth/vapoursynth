3659889dac74ab316d21261f34eb17949d487ab1
Fredrik Mellbin <fredrik.mellbin@gmail.com>
2014-03-04 11:41:35 +0100

    Fix typo in lachs0r's test fix, add test for old argument reordering

--- test/test.py
+++ test/test.py
@@ -65,8 +65,7 @@ class CoreTestSequence(unittest.TestCase):
 
     def test_arg14(self):
         clip = self.core.std.BlankClip(format=vs.YUV420P8, color=[69, 242, 115])
-        with self.assertRaises(vs.Error):
-            self.core.text.ClipInfo(clip, alignment=None)
+        self.core.text.ClipInfo(clip, alignment=None)
 
 #lut argument tests
     def test_lut_arg1(self):
@@ -123,9 +122,14 @@ class CoreTestSequence(unittest.TestCase):
         with self.assertRaises(vs.Error):
             self.core.std.Lut2(clip, clip)
 
-    def test_lut2_arg3(self):
+    def test_lut2_arg3_1(self):
         clip = self.core.std.BlankClip(format=vs.YUV420P8, color=[69, 242, 115])
         self.core.std.Lut2(clip, clip, lut=[5]*65536)
+
+    def test_lut2_arg3_2(self):
+        # this is intentional to test old argument order rewrite
+        clip = self.core.std.BlankClip(format=vs.YUV420P8, color=[69, 242, 115])
+        self.core.std.Lut2([clip, clip], lut=[5]*65536)
 
     def test_lut2_arg4(self):
         clip = self.core.std.BlankClip(format=vs.YUV420P8, color=[69, 242, 115])
