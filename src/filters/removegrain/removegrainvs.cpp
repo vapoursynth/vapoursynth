843455167d291c6703112015ec79332c6bddbed3
Fredrik Mellbin <fredrik.mellbin@gmail.com>
2014-03-09 19:22:10 +0100

    Fix signedness typo in rgvs mode 20

--- src/filters/removegrain/removegrainvs.cpp
+++ src/filters/removegrain/removegrainvs.cpp
@@ -994,7 +994,7 @@ private:
 
 class OpRG20 : public LineProcAll {
 public:
-    typedef    ConvSigned    ConvSign;
+    typedef    ConvUnsigned    ConvSign;
     static __forceinline int rg (int c, int a1, int a2, int a3, int a4, int a5, int a6, int a7, int a8) {
         const int        sum = a1 + a2 + a3 + a4 + c + a5 + a6 + a7 + a8;
         const int        val = (sum + 4) / 9;
