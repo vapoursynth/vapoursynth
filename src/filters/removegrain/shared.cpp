f4ea6935e4ccce397883c4b5d5569f676c773fbc
Fredrik Mellbin <fredrik.mellbin@gmail.com>
2014-11-01 19:49:35 +0100

    More consistent nullptr use

--- src/filters/removegrain/shared.cpp
+++ src/filters/removegrain/shared.cpp
@@ -21,8 +21,8 @@ http://sam.zoy.org/wtfpl/COPYING for more details.
 
 VS_EXTERNAL_API(void) VapourSynthPluginInit(VSConfigPlugin configFunc, VSRegisterFunction registerFunc, VSPlugin *plugin) {
     configFunc("com.vapoursynth.removegrainvs", "rgvs", "RemoveGrain VapourSynth Port", VAPOURSYNTH_API_VERSION, 1, plugin);
-    registerFunc("RemoveGrain", "clip:clip;mode:int[];", removeGrainCreate, 0, plugin);
-    registerFunc("Repair", "clip:clip;repairclip:clip;mode:int[];", repairCreate, 0, plugin);
+    registerFunc("RemoveGrain", "clip:clip;mode:int[];", removeGrainCreate, nullptr, plugin);
+    registerFunc("Repair", "clip:clip;repairclip:clip;mode:int[];", repairCreate, nullptr, plugin);
     registerFunc("Clense", "clip:clip;previous:clip:opt;next:clip:opt;planes:int[]:opt;", clenseCreate, reinterpret_cast<void *>(cmNormal), plugin);
     registerFunc("ForwardClense", "clip:clip;planes:int[]:opt;", clenseCreate, reinterpret_cast<void *>(cmForward), plugin);
     registerFunc("BackwardClense", "clip:clip;planes:int[]:opt;", clenseCreate, reinterpret_cast<void *>(cmBackward), plugin);
