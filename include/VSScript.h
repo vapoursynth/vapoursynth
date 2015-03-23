adec207c4be4123f0d4bcc3454dc36152c0b8659
Fredrik Mellbin <fredrik.mellbin@gmail.com>
2015-03-23 09:10:13 +0100

    make vsscript_getError check for null handle, add copyright notice

--- include/VSScript.h
+++ include/VSScript.h
@@ -1,5 +1,5 @@
 /*
-* Copyright (c) 2013-2014 Fredrik Mellbin
+* Copyright (c) 2013-2015 Fredrik Mellbin
 *
 * This file is part of VapourSynth.
 *
--- src/vsscript/vsscript.cpp
+++ src/vsscript/vsscript.cpp
@@ -1,3 +1,23 @@
+/*
+* Copyright (c) 2013-2015 Fredrik Mellbin
+*
+* This file is part of VapourSynth.
+*
+* VapourSynth is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
+*
+* VapourSynth is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public
+* License along with VapourSynth; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+*/
+
 #include "VapourSynth.h"
 #include "VSScript.h"
 #include "cython/vapoursynth_api.h"
@@ -90,7 +110,10 @@ VS_API(void) vsscript_freeScript(VSScript *handle) {
 }
 
 VS_API(const char *) vsscript_getError(VSScript *handle) {
-    return vpy_getError(handle);
+    if (handle)
+        return vpy_getError(handle);
+    else
+        return "Invalid handle (NULL)";
 }
 
 VS_API(VSNodeRef *) vsscript_getOutput(VSScript *handle, int index) {
