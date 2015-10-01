aa6ae2fad9a61378ef0fb5a16b64d846a0226f75
Fredrik Mellbin <fredrik.mellbin@gmail.com>
2015-10-01 17:59:55 +0200

    Make cython always rebuild modules on windows

--- cython_build.bat
+++ cython_build.bat
@@ -1,2 +1,5 @@
+rmdir /s /q build
+del vapoursynth.*.pyd
 "C:\Program Files (x86)\Python 3.5\python.exe" setup.py build_ext --inplace
 "C:\Program Files\Python 3.5\python.exe" setup.py build_ext --inplace
+pause
