db888161db4676a02f0359bff3e5c614ef23b9e2
Fredrik Mellbin <fredrik.mellbin@gmail.com>
2016-01-14 00:35:48 +0100

    Changelog and copyright year

--- src/core/version.h
+++ src/core/version.h
@@ -1,5 +1,5 @@
 /*
-* Copyright (c) 2012-2015 Fredrik Mellbin
+* Copyright (c) 2012-2016 Fredrik Mellbin
 *
 * This file is part of VapourSynth.
 *
@@ -33,7 +33,7 @@
 #define VS_OPTIONS_TEXT "Options: -\n"
 #endif
 #define VAPOURSYNTH_VERSION_STRING "VapourSynth Video Processing Library\n" \
-    "Copyright (c) 2012-2015 Fredrik Mellbin\n" \
+    "Copyright (c) 2012-2016 Fredrik Mellbin\n" \
     "Core R" XSTR(VAPOURSYNTH_CORE_VERSION) "\n" \
     "API R" XSTR(VAPOURSYNTH_API_MAJOR) "." XSTR(VAPOURSYNTH_API_MINOR) "\n" \
     VS_OPTIONS_TEXT
