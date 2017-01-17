29419cf81068183029b1dc63e718937de155a754
myrsloik <fredrik.mellbin@gmail.com>
2017-01-17 23:48:51 +0100

    Fix one more deprecation warning

--- test/weakref_test.py
+++ test/weakref_test.py
@@ -10,18 +10,18 @@ class FilterTestSequence(unittest.TestCase):
 
     def test_weakref_core(self):
         ref = weakref.ref(self.core)
-        self.assert_(ref() is self.core)
+        self.assertTrue(ref() is self.core)
 
     def test_weakref_node(self):
         video = self.core.std.BlankClip()
         ref = weakref.ref(video)
-        self.assert_(ref() is video)
+        self.assertTrue(ref() is video)
 
     def test_weakref_frame(self):
         video = self.core.std.BlankClip()
         frame = video.get_frame(0)
         ref = weakref.ref(frame)
-        self.assert_(ref() is frame)
+        self.assertTrue(ref() is frame)
 
 if __name__ == '__main__':
     unittest.main()
