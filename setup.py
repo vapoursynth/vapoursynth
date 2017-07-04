2bcbeb4bdd1a569c11acc2734c00cd90e1e4a655
stux! <stuxcrystal@encode.moe>
2017-07-05 00:59:13 +0200

    Fix the installation

--- setup.py
+++ setup.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 
 from platform import architecture
-from os import curdir, pardir
+from os import curdir
 from os.path import join
-from distutils.core import setup
-from Cython.Distutils import Extension, build_ext
+from setuptools import setup, Extension
 
 is_win = (architecture()[1] == "WindowsPE")
 is_64 = (architecture()[0] == "64bit")
@@ -13,8 +12,33 @@ library_dirs = [curdir, "build"]
 if is_win:
     if is_64:
         library_dirs.append(join("msvc_project", "x64", "Release"))
+        lib_suffix = "lib64"
     else:
         library_dirs.append(join("msvc_project", "Release"))
+        lib_suffix = "lib32"
+
+    #
+    # This code detects the library directory by querying the Windows Registry
+    # for the current VapourSynth directory location.
+    #
+
+    import winreg
+    REGISTRY_PATH = r"SOFTWARE\VapourSynth"
+    REGISTRY_KEY = r"Path"
+
+    def query(hkey, path, key):
+        reg_key = None
+        try:
+            reg_key = winreg.OpenKey(hkey, path, 0, winreg.KEY_READ)
+            value, _ = winreg.QueryValueEx(reg_key, key)
+        finally:
+            if reg_key is not None:
+                winreg.CloseKey(reg_key)
+
+        return value
+
+    library_dirs.append(join(query(winreg.HKEY_LOCAL_MACHINE, REGISTRY_PATH, REGISTRY_KEY), "sdk", lib_suffix))
+
 
 setup(
     name = "VapourSynth",
@@ -27,10 +51,13 @@ setup(
     version = "1.0.0",
     long_description = "A portable replacement for Avisynth",
     platforms = "All",
-    cmdclass = {'build_ext': build_ext},
     ext_modules = [Extension("vapoursynth", [join("src", "cython", "vapoursynth.pyx")],
                              libraries = ["vapoursynth"],
                              library_dirs = library_dirs,
                              include_dirs = [curdir, join("src", "cython")],
-                             cython_c_in_temp = 1)]
+                             cython_c_in_temp = 1)],
+    setup_requires=[
+        'setuptools>=18.0',
+        "Cython",
+    ]
 )
