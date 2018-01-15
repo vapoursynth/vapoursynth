a4ffbfa5204a2eeaba7291881064d0c6ba5ebc3e
Fredrik Mellbin <fredrik.mellbin@gmail.com>
2018-01-15 23:22:42 +0100

    Add general filter test file

---
+++ test/filter_test.py
@@ -0,0 +1,28 @@
+import unittest
+import vapoursynth as vs
+
+class FilterTestSequence(unittest.TestCase):
+
+    def setUp(self):
+        self.core = vs.get_core()
+        self.Transpose = self.core.std.Transpose
+        self.BlankClip = self.core.std.BlankClip
+		
+    def test_transpose8_test(self):
+        clip = self.BlankClip(format=vs.YUV420P8, color=[0, 0, 0], width=1156, height=752)
+        self.Transpose(clip).get_frame(0)
+
+    def test_transpose16(self):
+        clip = self.BlankClip(format=vs.YUV420P16, color=[0, 0, 0], width=1156, height=752)
+        self.Transpose(clip).get_frame(0)
+
+    def test_transpose32(self):
+        clip = self.BlankClip(format=vs.YUV420P32, color=[0, 0, 0], width=1156, height=752)
+        self.Transpose(clip).get_frame(0)
+
+    def test_transposeS(self):
+        clip = self.BlankClip(format=vs.YUV444PS, color=[0, 0, 0], width=1156, height=752)
+        self.Transpose(clip).get_frame(0)
+
+if __name__ == '__main__':
+    unittest.main()
