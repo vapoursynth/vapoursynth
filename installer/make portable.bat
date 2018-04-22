c5aeb3bf3f504912571b5e0d8acbae01d11abaad
Fredrik Mellbin <fredrik.mellbin@gmail.com>
2018-04-22 16:46:56 +0200

    Update visual studio runtime path

--- installer/make portable.bat
+++ installer/make portable.bat
@@ -1,4 +1,4 @@
-em extract version string
+rem extract version string
 for /F "tokens=2 delims='" %%a in ('findstr /C:"#define Version" vsinstaller.iss') do set v=%%a
 @echo %v%
 
@@ -18,7 +18,7 @@ copy ..\msvc_project\x64\Release\Morpho.dll buildp64\vapoursynth64\coreplugins
 copy ..\msvc_project\x64\Release\RemoveGrainVS.dll buildp64\vapoursynth64\coreplugins
 copy ..\msvc_project\x64\Release\Vinverse.dll buildp64\vapoursynth64\coreplugins
 copy ..\msvc_project\x64\Release\VIVTC.dll buildp64\vapoursynth64\coreplugins
-copy "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Redist\MSVC\14.12.25810\x64\Microsoft.VC141.CRT\*" buildp64
+copy "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Redist\MSVC\14.13.26020\x64\Microsoft.VC141.CRT\*" buildp64
 copy x64\plugins\* buildp64\vapoursynth64\coreplugins
 copy .\setup.py buildp64
 copy .\MANIFEST.in buildp64
@@ -46,7 +46,7 @@ copy ..\msvc_project\Release\Morpho.dll buildp32\vapoursynth32\coreplugins
 copy ..\msvc_project\Release\RemoveGrainVS.dll buildp32\vapoursynth32\coreplugins
 copy ..\msvc_project\Release\Vinverse.dll buildp32\vapoursynth32\coreplugins
 copy ..\msvc_project\Release\VIVTC.dll buildp32\vapoursynth32\coreplugins
-copy "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Redist\MSVC\14.12.25810\x86\Microsoft.VC141.CRT\*" buildp32
+copy "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Redist\MSVC\14.13.26020\x86\Microsoft.VC141.CRT\*" buildp32
 copy x86\plugins\* buildp32\vapoursynth32\coreplugins
 copy .\setup.py buildp32
 copy .\MANIFEST.in buildp32
