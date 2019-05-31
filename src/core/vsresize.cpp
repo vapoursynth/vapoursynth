be61b294187623ca0e4f8e038d57f8cbdfb31fa2
Fredrik Mellbin <fredrik.mellbin@gmail.com>
2019-05-31 19:56:37 +0200

    Fix compilation with zimg master

--- src/core/vsresize.cpp
+++ src/core/vsresize.cpp
@@ -85,7 +85,7 @@ const std::unordered_map<std::string, zimg_cpu_type_e> g_cpu_type_table{
     { "f16c",      ZIMG_CPU_X86_F16C },
     { "avx2",      ZIMG_CPU_X86_AVX2 },
     { "avx512f",   ZIMG_CPU_X86_AVX512F },
-    { "avx512skl", ZIMG_CPU_X86_AVX512_SKL },
+    { "avx512skx", ZIMG_CPU_X86_AVX512_SKX },
 #endif
 };
 
