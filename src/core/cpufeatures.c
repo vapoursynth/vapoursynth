5bc0fad20a052a4ae8640827c2f95118d1643bd4
Youka <spanknebel.borken@t-online.de>
2019-08-09 23:42:34 +0200

    optimized cpu features detection (less memory, more human-readable)

--- src/core/cpufeatures.c
+++ src/core/cpufeatures.c
@@ -25,16 +25,38 @@
 #ifdef VS_TARGET_CPU_X86
 
 #ifdef _MSC_VER
-#include <intrin.h>
+    #include <intrin.h>
+    /* ecx */
+    #define bit_SSE3	(1 << 0)
+    #define bit_SSSE3	(1 << 9)
+    #define bit_FMA		(1 << 12)
+    #define bit_SSE4_1	(1 << 19)
+    #define bit_SSE4_2	(1 << 20)
+    #define bit_MOVBE	(1 << 22)
+    #define bit_POPCNT	(1 << 23)
+    #define bit_AES		(1 << 25)
+    #define bit_OSXSAVE	(1 << 27)
+    #define bit_AVX		(1 << 28)
+    #define bit_F16C	(1 << 29)
+    /* edx */
+    #define bit_SSE2	(1 << 26)
+    /* Extended Features (%eax == 7) */
+    /* ebx */
+    #define bit_AVX2	(1 << 5)
+    #define bit_AVX512F	(1 << 16)
+    #define bit_AVX512DQ	(1 << 17)
+    #define bit_AVX512CD	(1 << 28)
+    #define bit_AVX512BW	(1 << 30)
+    #define bit_AVX512VL	(1u << 31)
 #else
-#include <cpuid.h>
+    #include <cpuid.h>
 #endif
 
+/* xgetbv mask (ecx) */
+#define _XCR_XFEATURE_ENABLED_MASK 0x0
+
 static void vs_cpu_cpuid(int index, int* eax, int* ebx, int* ecx, int* edx) {
-    *eax = 0;
-    *ebx = 0;
-    *ecx = 0;
-    *edx = 0;
+    *eax = *ebx = *ecx = *edx = 0;
 #ifdef _MSC_VER
     int regs[4];
     __cpuidex(regs, index, 0);
@@ -45,7 +67,7 @@ static void vs_cpu_cpuid(int index, int* eax, int* ebx, int* ecx, int* edx) {
 #elif defined(__GNUC__)
     __cpuid_count(index, 0, *eax, *ebx, *ecx, *edx);
 #else
-#error Unknown compiler, can't get cpuid
+#error Unknown compiler, cannot get cpuid
 #endif
 }
 
@@ -64,40 +86,33 @@ static unsigned long long vs_cpu_xgetbv(unsigned ecx) {
 void getCPUFeatures(CPUFeatures *cpuFeatures) {
     memset(cpuFeatures, 0, sizeof(CPUFeatures));
 
-    int eax = 0;
-    int ebx = 0;
-    int ecx = 0;
-    int edx = 0;
-    long long xedxeax = 0;
+    int eax, ebx, ecx, edx;
+    long long xedxeax;
     vs_cpu_cpuid(1, &eax, &ebx, &ecx, &edx);
-    cpuFeatures->can_run_vs = !!(edx & (1 << 26)); //sse2
-    cpuFeatures->sse3 = !!(ecx & 1);
-    cpuFeatures->ssse3 = !!(ecx & (1 << 9));
-    cpuFeatures->sse4_1 = !!(ecx & (1 << 19));
-    cpuFeatures->sse4_2 = !!(ecx & (1 << 20));
-    cpuFeatures->fma3 = !!(ecx & (1 << 12));
-    cpuFeatures->f16c = !!(ecx & (1 << 29));
-    cpuFeatures->aes = !!(ecx & (1 << 25));
-    cpuFeatures->movbe = !!(ecx & (1 << 22));
-    cpuFeatures->popcnt = !!(ecx & (1 << 23));
+    cpuFeatures->can_run_vs = !!(edx & bit_SSE2);
+    cpuFeatures->sse3 = !!(ecx & bit_SSE3);
+    cpuFeatures->ssse3 = !!(ecx & bit_SSSE3);
+    cpuFeatures->sse4_1 = !!(ecx & bit_SSE4_1);
+    cpuFeatures->sse4_2 = !!(ecx & bit_SSE4_2);
+    cpuFeatures->fma3 = !!(ecx & bit_FMA);
+    cpuFeatures->f16c = !!(ecx & bit_F16C);
+    cpuFeatures->aes = !!(ecx & bit_AES);
+    cpuFeatures->movbe = !!(ecx & bit_MOVBE);
+    cpuFeatures->popcnt = !!(ecx & bit_POPCNT);
 
-    if ((ecx & (1 << 27)) && (ecx & (1 << 28))) {
-        xedxeax = vs_cpu_xgetbv(0);       
-        cpuFeatures->avx = ((xedxeax & 0x06) == 0x06);
+    if ((ecx & bit_OSXSAVE) && (ecx & bit_AVX)) {
+        xedxeax = vs_cpu_xgetbv(_XCR_XFEATURE_ENABLED_MASK);
+        cpuFeatures->avx = !!(xedxeax & 0x06);
         if (cpuFeatures->avx) {
-            eax = 0;
-            ebx = 0;
-            ecx = 0;
-            edx = 0;
             vs_cpu_cpuid(7, &eax, &ebx, &ecx, &edx);
-            cpuFeatures->avx2 = !!(ebx & (1 << 5));
-            cpuFeatures->avx512_f = !!(ebx & (1 << 16)) && ((xedxeax & 0xE0) == 0xE0);
+            cpuFeatures->avx2 = !!(ebx & bit_AVX2);
+            cpuFeatures->avx512_f = !!(ebx & bit_AVX512F) && !!(xedxeax & 0xE0);
 
             if (cpuFeatures->avx512_f) {
-                cpuFeatures->avx512_cd = !!(ebx & (1 << 28));
-                cpuFeatures->avx512_bw = !!(ebx & (1 << 30));
-                cpuFeatures->avx512_dq = !!(ebx & (1 << 17));
-                cpuFeatures->avx512_vl = !!(ebx & (1 << 31));
+                cpuFeatures->avx512_cd = !!(ebx & bit_AVX512CD);
+                cpuFeatures->avx512_bw = !!(ebx & bit_AVX512BW);
+                cpuFeatures->avx512_dq = !!(ebx & bit_AVX512DQ);
+                cpuFeatures->avx512_vl = !!(ebx & bit_AVX512VL);
             }
         }
     }
@@ -111,7 +126,6 @@ void getCPUFeatures(CPUFeatures *cpuFeatures) {
     unsigned long long hwcap = getauxval(AT_HWCAP);
 
     cpuFeatures->can_run_vs = 1;
-
 #ifdef VS_TARGET_CPU_ARM
     cpuFeatures->half_fp = !!(hwcap & HWCAP_ARM_HALF);
     cpuFeatures->edsp = !!(hwcap & HWCAP_ARM_EDSP);
@@ -127,9 +141,9 @@ void getCPUFeatures(CPUFeatures *cpuFeatures) {
     cpuFeatures->dfp = !!(hwcap & PPC_FEATURE_HAS_DFP);
     cpuFeatures->vsx = !!(hwcap & PPC_FEATURE_HAS_VSX);
 #else
-#warning "Do not know how to get CPU features on Linux."
+    #warning "Do not know how to get CPU features on Linux."
 #endif
 }
 #else
-#warning "Do not know how to get CPU features."
+    #warning "Do not know how to get CPU features."
 #endif
--- src/core/cpufeatures.h
+++ src/core/cpufeatures.h
@@ -21,53 +21,55 @@
 #ifndef CPUFEATURES_H
 #define CPUFEATURES_H
 
-typedef struct CPUFeatures {
+#define FLAG_FIELD(name) unsigned short name : 1
+
+typedef struct {
     // This is to determine if the cpu is up to the minimum requirements in terms of supported instructions
     // that the VapourSynth core uses.
-    char can_run_vs;
+    FLAG_FIELD(can_run_vs);
 #ifdef VS_TARGET_CPU_X86
     // On x86, all features up to sse2 are required.
-    char sse3;
-    char ssse3;
-    char sse4_1;
-    char sse4_2;
-    char fma3;
-    char avx;
-    char avx2;
-    char f16c;
-    char aes;
-    char movbe;
-    char popcnt;
-    char avx512_f;
-    char avx512_cd;
-    char avx512_bw;
-    char avx512_dq;
-    char avx512_vl;
+    FLAG_FIELD(sse3);
+    FLAG_FIELD(ssse3);
+    FLAG_FIELD(sse4_1);
+    FLAG_FIELD(sse4_2);
+    FLAG_FIELD(fma3);
+    FLAG_FIELD(avx);
+    FLAG_FIELD(avx2);
+    FLAG_FIELD(f16c);
+    FLAG_FIELD(aes);
+    FLAG_FIELD(movbe);
+    FLAG_FIELD(popcnt);
+    FLAG_FIELD(avx512_f);
+    FLAG_FIELD(avx512_cd);
+    FLAG_FIELD(avx512_bw);
+    FLAG_FIELD(avx512_dq);
+    FLAG_FIELD(avx512_vl);
 #elif defined(VS_TARGET_CPU_ARM)
     // On ARM, VFP-D16+ (16 double registers or more) is required.
-    char half_fp;
-    char edsp;
-    char iwmmxt;
-    char neon;
-    char fast_mult;
-    char idiv_a;
+    FLAG_FIELD(half_fp);
+    FLAG_FIELD(edsp);
+    FLAG_FIELD(iwmmxt);
+    FLAG_FIELD(neon);
+    FLAG_FIELD(fast_mult);
+    FLAG_FIELD(idiv_a);
 #elif defined(VS_TARGET_CPU_POWERPC)
     // On PowerPC, FPU and MMU are required.
-    char altivec;
-    char spe;
-    char efp_single;
-    char efp_double;
-    char dfp;
-    char vsx;
+    FLAG_FIELD(altivec);
+    FLAG_FIELD(spe);
+    FLAG_FIELD(efp_single);
+    FLAG_FIELD(efp_double);
+    FLAG_FIELD(dfp);
+    FLAG_FIELD(vsx);
 #else
-#warning "No VS_TARGET_CPU_* defined/handled!"
+    #warning "No VS_TARGET_CPU_* defined/handled!"
 #endif
 } CPUFeatures;
 
 #ifdef __cplusplus
-#define CPU_FEATURES_EXTERN_C extern "C"
+    #define CPU_FEATURES_EXTERN_C extern "C"
 #else
-#define CPU_FEATURES_EXTERN_C
+    #define CPU_FEATURES_EXTERN_C
 #endif
 
 CPU_FEATURES_EXTERN_C void getCPUFeatures(CPUFeatures *cpuFeatures);
