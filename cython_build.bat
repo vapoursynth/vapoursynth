01468ec8d2bc60de3dc724fe5972f0918fda5e88
Youka <spanknebel.borken@t-online.de>
2019-08-09 13:12:45 +0200

    added appveyor configuration

--- cython_build.bat
+++ cython_build.bat
@@ -3,7 +3,11 @@ rmdir /s /q build 2>nul
 del /q vapoursynth.*.pyd 2>nul
 del /q dist\*.whl 2>nul
 :: Build
-"C:\Program Files (x86)\Python37-32\python.exe" setup.py build_ext --inplace
-"C:\Program Files\Python37\python.exe" setup.py build_ext --inplace
-"C:\Program Files (x86)\Python37-32\python.exe" setup.py bdist_wheel
-"C:\Program Files\Python37\python.exe" setup.py bdist_wheel
+if not "%PLATFORM%" == "x64" (
+    "%PYTHON32%/python.exe" setup.py build_ext --inplace
+    "%PYTHON32%/python.exe" setup.py bdist_wheel
+)
+if not "%PLATFORM%" == "Win32" (
+    "%PYTHON64%/python.exe" setup.py build_ext --inplace
+    "%PYTHON64%/python.exe" setup.py bdist_wheel
+)
