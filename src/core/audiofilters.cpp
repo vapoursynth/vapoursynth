2292fc64a371110906735f1acf4fee6a8a73c941
Fredrik Mellbin <fredrik.mellbin@gmail.com>
2020-02-12 22:26:11 +0100

    Fix compilation

--- src/core/audiofilters.cpp
+++ src/core/audiofilters.cpp
@@ -61,27 +61,6 @@ static const VSFrameRef *VS_CC audioMixGetframe(int n, int activationReason, voi
 
         T maxval = static_cast<T>((static_cast<int64_t>(1) << fi->bitsPerSample) - 1);
 
-        for (int plane = 0; plane < fi->numPlanes; plane++) {
-
-            if (d->process[plane]) {
-                const T * VS_RESTRICT srcp = reinterpret_cast<const T *>(vsapi->getReadPtr(src, plane));
-                int src_stride = vsapi->getStride(src, plane);
-                U * VS_RESTRICT dstp = reinterpret_cast<U *>(vsapi->getWritePtr(dst, plane));
-                int dst_stride = vsapi->getStride(dst, plane);
-                int h = vsapi->getFrameHeight(src, plane);
-                int w = vsapi->getFrameWidth(src, plane);
-
-                const U * VS_RESTRICT lut = reinterpret_cast<const U *>(d->lut);
-
-                for (int hl = 0; hl < h; hl++) {
-                    for (int x = 0; x < w; x++)
-                        dstp[x] =  lut[std::min(srcp[x], maxval)];
-
-                    dstp += dst_stride / sizeof(U);
-                    srcp += src_stride / sizeof(T);
-                }
-            }
-        }
 
         vsapi->freeFrame(src);
         return dst;
@@ -91,12 +70,55 @@ static const VSFrameRef *VS_CC audioMixGetframe(int n, int activationReason, voi
 }
 
 static void VS_CC audioMixFree(void *instanceData, VSCore *core, const VSAPI *vsapi) {
-    LutData *d = reinterpret_cast<LutData *>(instanceData);
-    d->freeNode = vsapi->freeNode;
+    AudioMixData *d = reinterpret_cast<AudioMixData *>(instanceData);
     delete d;
 }
 
 static void VS_CC audioMixCreate(const VSMap *in, VSMap *out, void *userData, VSCore *core, const VSAPI *vsapi) {
+
+}
+
+
+//////////////////////////////////////////
+// ShuffleChannels
+
+struct ShuffleChannelsData {
+    VSNodeRef *node;
+    VSAudioInfo ai;
+};
+
+
+template<typename T>
+static const VSFrameRef *VS_CC shuffleChannelsGetframe(int n, int activationReason, void **instanceData, void **frameData, VSFrameContext *frameCtx, VSCore *core, const VSAPI *vsapi) {
+    ShuffleChannelsData *d = reinterpret_cast<ShuffleChannelsData *>(*instanceData);
+
+    if (activationReason == arInitial) {
+        vsapi->requestFrameFilter(n, d->node, frameCtx);
+    } else if (activationReason == arAllFramesReady) {
+        const VSFrameRef *src = vsapi->getFrameFilter(n, d->node, frameCtx);
+        const VSAudioFormat *fi = vsapi->getAudioFrameFormat(src);
+        size_t inChannels = fi->numChannels;
+
+        VSFrameRef *dst = vsapi->newAudioFrame(d->ai.format, d->ai.sampleRate, vsapi->getFrameLength(src), src, core);
+
+
+
+        T maxval = static_cast<T>((static_cast<int64_t>(1) << fi->bitsPerSample) - 1);
+
+
+        vsapi->freeFrame(src);
+        return dst;
+    }
+
+    return nullptr;
+}
+
+static void VS_CC shuffleChannelsFree(void *instanceData, VSCore *core, const VSAPI *vsapi) {
+    ShuffleChannelsData *d = reinterpret_cast<ShuffleChannelsData *>(instanceData);
+    delete d;
+}
+
+static void VS_CC shuffleChannelsCreate(const VSMap *in, VSMap *out, void *userData, VSCore *core, const VSAPI *vsapi) {
 
 }
 
@@ -105,6 +127,6 @@ static void VS_CC audioMixCreate(const VSMap *in, VSMap *out, void *userData, VS
 
 void VS_CC audioInitialize(VSConfigPlugin configFunc, VSRegisterFunction registerFunc, VSPlugin *plugin) {
     //configFunc("com.vapoursynth.std", "std", "VapourSynth Core Functions", VAPOURSYNTH_API_VERSION, 1, plugin);
-    registerFunc("AudioMix", "clip:anode[];matrix:int[]:opt;channels_out:int[];", audioMixCreate, 0, plugin);
-    registerFunc("ShuffleChannels", "clip:anode[];channels_in:int[];channels_out:int[]:opt;", shuffleChannelsCreate, 0, plugin);
+    registerFunc("AudioMix", "clip:anode;matrix:int[]:opt;channels_out:int[];", audioMixCreate, 0, plugin);
+    registerFunc("ShuffleChannels", "clip:anode[];channels_in:int[];channels_out:int[];", shuffleChannelsCreate, 0, plugin);
 }
