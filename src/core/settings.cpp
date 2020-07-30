a644716f48361bdf79b66ff0cab71a8c92051b79
Fredrik Mellbin <fredrik.mellbin@gmail.com>
2020-07-30 14:38:36 +0200

    Probably fix non-windows compilation

--- src/core/settings.cpp
+++ src/core/settings.cpp
@@ -28,14 +28,14 @@ VSMap *readSettings(const std::string &path) {
         // Ignore "No such file or directory"
         if (errno != ENOENT) {
             err.append("Couldn't open '").append(path).append("' for reading. Error: ").append(strerror(errno));
-            vs_internal_vsapi.setError(settings, err.c_str());
+            vs_internal_vsapi.mapSetError(settings, err.c_str());
         }
         return settings;
     }
 
     if (fseek(f, 0, SEEK_END)) {
         err.append("Couldn't find the size of '").append(path).append("' by seeking to its end. Error: ").append(strerror(errno));
-        vs_internal_vsapi.setError(settings, err.c_str());
+        vs_internal_vsapi.mapSetError(settings, err.c_str());
         fclose(f);
         return settings;
     }
@@ -43,14 +43,14 @@ VSMap *readSettings(const std::string &path) {
     long size = ftell(f);
     if (size == -1) {
         err.append("Couldn't find the size of '").append(path).append("'. ftell failed with the error: ").append(strerror(errno));
-        vs_internal_vsapi.setError(settings, err.c_str());
+        vs_internal_vsapi.mapSetError(settings, err.c_str());
         fclose(f);
         return settings;
     }
 
     if (size > 100*1024) {
         err.append("Configuration file '").append(path).append("' is ridiculously large. Ignoring.");
-        vs_internal_vsapi.setError(settings, err.c_str());
+        vs_internal_vsapi.mapSetError(settings, err.c_str());
         fclose(f);
         return settings;
     }
@@ -61,7 +61,7 @@ VSMap *readSettings(const std::string &path) {
 
     if (fread(buffer.data(), 1, size, f) != static_cast<size_t>(size)) {
         err.append("Didn't read the expected number of bytes from '").append(path).append("'.");
-        vs_internal_vsapi.setError(settings, err.c_str());
+        vs_internal_vsapi.mapSetError(settings, err.c_str());
         fclose(f);
         return settings;
     }
@@ -86,7 +86,7 @@ VSMap *readSettings(const std::string &path) {
             case WANT_KEY_CHAR:
                 if (buffer[i] == '\n') {
                     err.append(line_str).append("No delimiter found before reaching the end of the line.");
-                    vs_internal_vsapi.setError(settings, err.c_str());
+                    vs_internal_vsapi.mapSetError(settings, err.c_str());
                     return settings;
                 } else if (buffer[i] == ' ') {
                     key_end = i - 1;
@@ -96,7 +96,7 @@ VSMap *readSettings(const std::string &path) {
                     state = WANT_VALUE_START;
                 } else if (!isAlphaNumUnderscore(buffer[i])) {
                     err.append(line_str).append("Garbage found inside key.");
-                    vs_internal_vsapi.setError(settings, err.c_str());
+                    vs_internal_vsapi.mapSetError(settings, err.c_str());
                     return settings;
                 }
                 break;
@@ -105,18 +105,18 @@ VSMap *readSettings(const std::string &path) {
                     state = WANT_VALUE_START;
                 } else if (buffer[i] == '\n') {
                     err.append(line_str).append("No delimiter found before reaching the end of the line.");
-                    vs_internal_vsapi.setError(settings, err.c_str());
+                    vs_internal_vsapi.mapSetError(settings, err.c_str());
                     return settings;
                 } else {
                     err.append(line_str).append("Expected '=' but found garbage instead.");
-                    vs_internal_vsapi.setError(settings, err.c_str());
+                    vs_internal_vsapi.mapSetError(settings, err.c_str());
                     return settings;
                 }
                 break;
             case WANT_VALUE_START:
                 if (buffer[i] == '\n') {
                     err.append(line_str).append("No value found for key before reaching the end of the line.");
-                    vs_internal_vsapi.setError(settings, err.c_str());
+                    vs_internal_vsapi.mapSetError(settings, err.c_str());
                     return settings;
                 }
                 if (buffer[i] != ' ') {
@@ -129,13 +129,13 @@ VSMap *readSettings(const std::string &path) {
                     value_end = i - 1;
                     std::string key(&buffer[key_start], key_end - key_start + 1);
                     std::string value(&buffer[value_start], value_end - value_start + 1);
-                    vs_internal_vsapi.propSetData(settings, key.c_str(), value.c_str(), value.size(), dtUtf8, paReplace);
+                    vs_internal_vsapi.mapSetData(settings, key.c_str(), value.c_str(), value.size(), dtUtf8, paReplace);
                     state = WANT_KEY_START;
                 }
                 break;
             default:
                 err.append(line_str).append("Shit broke. This should never happen.");
-                vs_internal_vsapi.setError(settings, err.c_str());
+                vs_internal_vsapi.mapSetError(settings, err.c_str());
                 return settings;
                 break;
         }
