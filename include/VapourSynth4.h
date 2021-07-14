3afe0079a13bd2958b633ea62eaacf348e8433c6
Fredrik Mellbin <fredrik.mellbin@gmail.com>
2021-07-14 10:03:12 +0200

    Add more constants

--- include/VapourSynth4.h
+++ include/VapourSynth4.h
@@ -164,6 +164,20 @@ typedef struct VSVideoFormat {
     int numPlanes; /* implicit from colorFamily */
 } VSVideoFormat;
 
+typedef enum VSColorRange {
+    crFullRange = 0,
+    crLimitedRange = 1
+} VSColorRange;
+
+typedef enum VSChromaLocation {
+    clLeft = 0,
+    clCenter = 1,
+    clTopLeft = 2,
+    clTop = 3,
+    clBottomLeft = 4,
+    clBottom = 5
+} VSChromaLocation;
+
 typedef enum VSAudioChannels {
     acFrontLeft           = 0,
     acFrontRight          = 1,
