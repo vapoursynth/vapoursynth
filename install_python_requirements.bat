d215801493efedf9b822a0a45a601a2a90a395cb
Ichunjo <ichunjo.le.terrible@gmail.com>
2021-10-04 14:37:53 +0200

    add python requirements getter

---
+++ install_python_requirements.bat
@@ -0,0 +1,4 @@
+py -3.9 -m pip install -r python-requirements.txt
+py -3.9-32 -m pip install -r python-requirements.txt
+py -3.8 -m pip install -r python-requirements.txt
+py -3.8-32 -m pip install -r python-requirements.txt
