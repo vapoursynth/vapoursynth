1211fb5bf772ec3c1b4f92da6233909be6e3c30c
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-08-24 15:35:49 +0200

    Add 32 bits python reqs install

--- install_deps.bat
+++ install_deps.bat
@@ -2,4 +2,5 @@ git clone https://github.com/AviSynth/AviSynthPlus
 git clone https://github.com/sekrit-twc/libp2p
 git clone https://github.com/vapoursynth/vsrepo
 git clone https://github.com/sekrit-twc/zimg --branch v3.0
-pip install -r python-requirements.txt
+py -3.10 -m pip install -r python-requirements.txt
+py -3.10-32 -m pip install -r python-requirements.txt
