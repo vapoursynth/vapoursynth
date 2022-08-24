cd5a0208f5f1121197a83d4fcdac7f5cee88c700
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-08-24 16:24:28 +0200

    Automatically copy 7zip into installer

--- install_deps.bat
+++ install_deps.bat
@@ -1,6 +1,30 @@
+@echo off
+
 git clone https://github.com/AviSynth/AviSynthPlus
 git clone https://github.com/sekrit-twc/libp2p
 git clone https://github.com/vapoursynth/vsrepo
 git clone https://github.com/sekrit-twc/zimg --branch v3.0
 py -3.10 -m pip install -r python-requirements.txt
 py -3.10-32 -m pip install -r python-requirements.txt
+
+SET ZFOLDER=C:\Program Files\7-Zip
+IF EXIST "%ZFOLDER%\7z.exe" GOTO copym
+
+SET ZFOLDER=D:\Program Files\7-Zip
+IF EXIST "%ZFOLDER%\7z.exe" GOTO copym
+
+SET ZFOLDER=C:\Program Files (x86)\7-Zip
+IF EXIST "%ZFOLDER%\7z.exe" GOTO copym
+
+SET ZFOLDER=D:\Program Files (x86)\7-Zip
+IF EXIST "%ZFOLDER%\7z.exe" GOTO copym
+
+GOTO end
+:copym
+copy "%ZFOLDER%\7z.exe" installer
+copy "%ZFOLDER%\7z.dll" installer
+GOTO end
+
+ECHO 7-zip not installed!
+
+:end
