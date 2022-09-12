85e4241fdd58c1581fef3fc1bef1adcbed2d8911
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-09-12 13:38:54 +0200

    Fix construct_signature

--- src/cython/vapoursynth.pyx
+++ src/cython/vapoursynth.pyx
@@ -489,7 +489,7 @@ def construct_signature(signature, return_signature, injected=None):
         if param
     )
 
-    if injected:
+    if injected and params:
         del params[0]
 
     return_annotations = list(
