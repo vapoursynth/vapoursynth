6643508895ab3c182dd6f930dc091cb7d1675d94
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-12-20 12:04:55 +0100

    Update vs-detect-python.bat

--- installer/vs-detect-python.bat
+++ installer/vs-detect-python.bat
@@ -1,15 +1,15 @@
 @echo off
 
+IF EXIST "Python310.dll" (
+    ECHO Python 3.10 detected
+    GOTO end
+)
+
 IF EXIST "Python38.dll" (
     ECHO Python 3.8 detected
     ECHO Switching to 3.8 support...
     COPY /Y "VSScriptPython38.dll" "VSScript.dll"
     ECHO Done
-    GOTO end
-)
-
-IF EXIST "Python310.dll" (
-    ECHO Python 3.10 detected
     GOTO end
 )
 
