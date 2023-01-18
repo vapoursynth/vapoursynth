7126aefc29464a34bbb09b2b63309c6dd0dff287
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2023-01-18 15:43:01 +0100

    Update Python paths in build

--- compile_all.bat
+++ compile_all.bat
@@ -18,6 +18,12 @@ GOTO endc
 
 :buildm
 pushd msvc_project
+
+FOR /F "tokens=*" %%g IN ('py -3.10 -c "import sys; import os; print(os.path.dirname(sys.executable))"') DO SET VSPYTHON310_PATH=%%g
+FOR /F "tokens=*" %%g IN ('py -3.10-32 -c "import sys; import os; print(os.path.dirname(sys.executable))"') DO SET VSPYTHON31032_PATH=%%g
+FOR /F "tokens=*" %%g IN ('py -3.8 -c "import sys; import os; print(os.path.dirname(sys.executable))"') DO SET VSPYTHON38_PATH=%%g
+FOR /F "tokens=*" %%g IN ('py -3.8-32 -c "import sys; import os; print(os.path.dirname(sys.executable))"') DO SET VSPYTHON3832_PATH=%%g
+
 "%MSBuildPTH%\Msbuild\Current\Bin\MSBuild.exe" VapourSynth.sln /t:Clean;Build /p:Configuration=Release /p:Platform=x64
 "%MSBuildPTH%\Msbuild\Current\Bin\MSBuild.exe" VapourSynth.sln /t:Clean;Build /p:Configuration=Release /p:Platform=Win32
 popd
