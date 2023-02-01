4f3babf67f99794811fc3e4aa92ae46e1e87a2ef
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2023-02-01 17:26:20 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,23 @@
 #!/usr/bin/env python
 
+from os import curdir
+from os.path import dirname, exists, join
 from platform import architecture
 from shutil import which
-from os import curdir
-from os.path import join, exists, dirname
-from setuptools import setup, Extension
 
-is_win = (architecture()[1] == "WindowsPE")
-is_64 = (architecture()[0] == "64bit")
+from setuptools import Extension, setup  # type: ignore
+
+bits, linkage = architecture()
+
+is_win = linkage == "WindowsPE"
+is_64 = bits == "64bit"
+is_portable = False
 
 extra_data = {}
 
 library_dirs = [curdir, "build"]
 
-is_portable = False
+
 if is_win:
     if is_64:
         library_dirs.append(join("msvc_project", "x64", "Release"))
@@ -43,6 +47,8 @@ if is_win:
 
         return value
 
+    dll_path: str | None
+
     # Locate the vapoursynth dll inside the library directories first
     # should we find it, it is a clear indicator that VapourSynth
     # has been compiled by the user.
@@ -62,14 +68,17 @@ if is_win:
             # If the vapoursynth.dll is not located in PATH, we then hit the registry
             try:
                 dll_path = query(winreg.HKEY_LOCAL_MACHINE, REGISTRY_PATH, REGISTRY_KEY)
-            except:
+            except Exception:
                 # Give up.
                 raise OSError("Couldn't detect vapoursynth installation path")
             else:
                 # Since the SDK is on a different directory than the DLL insert the SDK to library_dirs
                 sdkpath = join(query(winreg.HKEY_LOCAL_MACHINE, REGISTRY_PATH, REGISTRY_KEY_PATH), "sdk", lib_suffix)
                 if not exists(sdkpath):
-                    raise OSError("It appears you don't have the sdk installed. Please make sure you installed the sdk before running setup.py")
+                    raise OSError(
+                        "It appears you don't have the sdk installed. "
+                        "Please make sure you installed the sdk before running setup.py"
+                    )
                 library_dirs.append(sdkpath)
 
         # Insert the DLL Path to the library dirs
@@ -78,34 +87,37 @@ if is_win:
 
     # Make sure the setup process copies the VapourSynth.dll into the site-package folder
     print("Found VapourSynth.dll at:", dll_path)
-    
+
     extra_data["data_files"] = [(r"Lib\site-packages", [dll_path])]
-        
-        
+
+
 setup(
-    name = "VapourSynth",
-    description = "A frameserver for the 21st century",
-    url = "http://www.vapoursynth.com/",
-    download_url = "https://github.com/vapoursynth/vapoursynth",
-    author = "Fredrik Mellbin",
-    author_email = "fredrik.mellbin@gmail.com",
-    license = "LGPL 2.1 or later",
-    version = "61",
-    long_description = "A portable replacement for Avisynth" if is_portable else "A modern replacement for Avisynth",
-    platforms = "All",
-    ext_modules = [Extension("vapoursynth", [join("src", "cython", "vapoursynth.pyx")],
-                             define_macros = [ ("VS_GRAPH_API", None) ],
-                             libraries = ["vapoursynth"],
-                             library_dirs = library_dirs,
-                             include_dirs = [
-                                 curdir,
-                                 join("src", "cython"),
-                                 join("src", "vsscript")
-                            ])],
+    name="VapourSynth",
+    description="A frameserver for the 21st century",
+    url="https://www.vapoursynth.com/",
+    download_url="https://github.com/vapoursynth/vapoursynth",
+    author="Fredrik Mellbin",
+    author_email="fredrik.mellbin@gmail.com",
+    license="LGPL 2.1 or later",
+    version="61",
+    long_description="A portable replacement for Avisynth" if is_portable else "A modern replacement for Avisynth",
+    platforms="All",
+    ext_modules=[
+        Extension(
+            "vapoursynth", [join("src", "cython", "vapoursynth.pyx")],
+            define_macros=[("VS_GRAPH_API", None)],
+            libraries=["vapoursynth"],
+            library_dirs=library_dirs,
+            include_dirs=[
+                curdir,
+                join("src", "cython"),
+                join("src", "vsscript")
+            ]
+        )
+    ],
     setup_requires=[
-        'setuptools>=18.0',
+        'setuptools>=58.2.0',
         "Cython",
     ],
-    
     **extra_data
 )
