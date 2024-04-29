241f934662369067e72777fdbd6a446541c77e3c
Fredrik Mellbin <fredrik.mellbin@gmail.com>
2024-04-29 20:17:41 +0200

    Don't upgrade pip, it usually fails on windows anyway and wastes a long time

--- install_deps.bat
+++ install_deps.bat
@@ -25,11 +25,9 @@ IF NOT EXIST zimg (
 )
 
 py -3.12 -m ensurepip
-py -3.12 -m pip install --upgrade pip --no-warn-script-location
 py -3.12 -m pip install --upgrade -r python-requirements.txt
 
 py -3.8 -m ensurepip
-py -3.8 -m pip install --upgrade pip --no-warn-script-location
 py -3.8 -m pip install --upgrade -r python-requirements.txt
 
 SET ZFOLDER=%ProgramFiles%\7-Zip
