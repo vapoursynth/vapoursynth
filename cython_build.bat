3f244a00da3ea7491de708bd94737376ce313abb
Fredrik Mellbin <fredrik.mellbin@gmail.com>
2025-06-03 16:44:32 +0200

    More fixes for python 3.8 build Give up on vapoursynth-portable

--- cython_build.bat
+++ cython_build.bat
@@ -2,5 +2,7 @@ rmdir /s /q build
 del vapoursynth.*.pyd
 del /q dist\*.whl
 py -3.13 -m build
+ren pyproject.toml pyproject_.toml
 py -3.8 setup.py bdist_wheel
+ren pyproject_.toml pyproject.toml
 pause
