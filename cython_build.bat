ae3595b223ef4e12a6bbc43633f7f4754ce874ac
Fredrik Mellbin <fredrik.mellbin@gmail.com>
2025-06-04 14:14:41 +0200

    Make pyproject.toml work with 3.8 as well

--- cython_build.bat
+++ cython_build.bat
@@ -2,7 +2,5 @@ rmdir /s /q build
 del vapoursynth.*.pyd
 del /q dist\*.whl
 py -3.13 -m build
-ren pyproject.toml pyproject_.toml
-py -3.8 setup.py bdist_wheel
-ren pyproject_.toml pyproject.toml
+py -3.8 -m build
 pause
