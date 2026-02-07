65e9102d3184a0870cc487bdedd8209b0db699bd
Fredrik Mellbin <fredrik.mellbin@gmail.com>
2026-02-07 17:48:20 +0100

    Probably fix windows CI

--- compile_all.bat
+++ compile_all.bat
@@ -8,7 +8,7 @@ pushd msvc_project
 
 FOR /F "tokens=*" %%g IN ('py -3.14 -c "import sys; import os; print(os.path.dirname(sys.executable))"') DO SET VSPYTHON_PATH=%%g
 
-"%MSBuildPTH%\Msbuild\Current\Bin\MSBuild.exe" -m /t:Clean;Build /p:Configuration=Release /p:Platform=x64 /p:CurrentVersion=%CURRENT_VERSION% VapourSynth.sln
+"%MSBuildPTH%\Msbuild\Current\Bin\MSBuild.exe" -m /t:Clean;Build /p:Configuration=Release /p:Platform=x64 /p:CurrentVersion=%CURRENT_VERSION% /p:PlatformToolset=v145 VapourSynth.sln
 
 popd
 
