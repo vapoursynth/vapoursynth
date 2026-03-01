9c0838696d088b8a9352cba2f2576282fb282ba3
HolyWu <holywu@gmail.com>
2026-03-01 10:38:40 +0800

    Fix conversion error on *nix

--- src/vsscript/vsscript.cpp
+++ src/vsscript/vsscript.cpp
@@ -68,7 +68,7 @@ static std::filesystem::path getLibraryPath() {
     return pathBuf.data();
 #else
     Dl_info info = {};
-    if (dladdr(&getLibraryPath, &info))
+    if (dladdr(reinterpret_cast<void*>(&getLibraryPath), &info))
         return info.dli_fname;
 #endif
     return {};
