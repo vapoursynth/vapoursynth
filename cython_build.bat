80e8b9a0b91a8f24fb3bdee85ab3cfbb43f2611b
HolyWu <holywu@gmail.com>
2026-03-02 18:46:59 +0800

    Fix wheel interpreter tag

--- cython_build.bat
+++ cython_build.bat
@@ -2,4 +2,7 @@ rmdir /s /q build
 del vapoursynth.*.pyd
 del /q dist\*.whl
 py -3.14 -m build --sdist --wheel
+pushd dist
+for %%i in (*.whl) do wheel tags --remove --python-tag cp312 %%i
+popd
 pause
