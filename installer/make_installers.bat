0487d1038a11fe978c368144f91edc766a22fa45
Fredrik Mellbin <fredrik.mellbin@gmail.com>
2026-07-15 22:27:52 +0200

    Use inno setup 7

--- installer/make_installers.bat
+++ installer/make_installers.bat
@@ -1,10 +1,10 @@
 call setmsvcvars.bat
 
 IF EXIST "%ProgramFiles(x86)%\Inno Setup 6\ISCC.exe" (
-    "%ProgramFiles(x86)%\Inno Setup 6\ISCC.exe" /DVSRuntimeVersion=%RedistVersion% /DVersion=%CURRENT_VERSION% /DVersionExtra=%CURRENT_VERSION_EXTRA% vsinstaller.iss
+    "%ProgramFiles(x86)%\Inno Setup 7\ISCC.exe" /DVSRuntimeVersion=%RedistVersion% /DVersion=%CURRENT_VERSION% /DVersionExtra=%CURRENT_VERSION_EXTRA% vsinstaller.iss
     GOTO end
 ) ELSE (
-    "C:\Program Files (x86)\Inno Setup 6\ISCC.exe" /DVSRuntimeVersion=%RedistVersion% /DVersion=%CURRENT_VERSION% /DVersionExtra=%CURRENT_VERSION_EXTRA% vsinstaller.iss
+    "C:\Program Files (x86)\Inno Setup 7\ISCC.exe" /DVSRuntimeVersion=%RedistVersion% /DVersion=%CURRENT_VERSION% /DVersionExtra=%CURRENT_VERSION_EXTRA% vsinstaller.iss
     GOTO end
 )
 
